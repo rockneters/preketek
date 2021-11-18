512b3584e80d378320efc64e1937fa8e0a330538
rockneters <89226199+rockneters@users.noreply.github.com>
2021-11-19 03:45:47 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,16 +12,7 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo "Authentikasi pada server"
-sleep 2
-IZIN=$( curl icanhazip.com | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permintaan Diterima...${NC}"
-else
-echo -e "${red}Permintaan Ditolak!${NC}";
-echo "Hanya untuk pengguna terdaftar"
-rm -f setup.sh
-exit 0
-fi
+sleep 1
 mkdir /var/lib/premium-script;
 mkdir /etc/v2ray;
 echo "Tolong masukan domain yang sudah dipointing agar v2ray service work"
